e10ca94c0e5427073cf24587cc244bfc8b24ec20
tanaka <kwzn@hotmail.co.jp>
2020-02-11 09:52:33 +0900

    GPUでDocker起動完了。launch_bash動作確認でgazebo動作確認完了。ホストがnvidiじゃないと動かないかも？

--- docker/launch_bash.sh
+++ docker/launch_bash.sh
@@ -4,20 +4,26 @@ xhost +local:root
 
 # docker run -it \
 docker run -it \
+--gpus all \
 --env="DISPLAY"  \
 --env="QT_X11_NO_MITSHM=1"  \
---env="TURTLEBOT3_MODEL=burger" \
+--env="ROS_WS=$HOME/catkin_ws" \
+--env="TURTLEBOT3_MODEL" \
+--env="GAZEBO_MODEL_PATH" \
+--env="ROS_MASTER_URI" \
 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
---workdir="/home/$USER" \
+--workdir="$HOME" \
 --volume="/etc/group:/etc/group:ro" \
 --volume="/etc/passwd:/etc/passwd:ro" \
 --volume="/etc/shadow:/etc/shadow:ro" \
 --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
 --volume="$HOME/host_docker:/home/user/host_docker" \
+--volume="$HOME/catkin_ws:$HOME/catkin_ws" \
+--volume="$HOME/.gazebo:/root/.gazebo" \
 -e LOCAL_USER_ID=`id -u $USER` \
 -e LOCAL_GROUP_ID=`id -g $USER` \
 -e LOCAL_GROUP_NAME=`id -gn $USER` \
- buger_war
+ ros_kinetic_gpu bash
 
 xhost -local:root
 
